0b95bdb287a433344cec96623bf587f7d078c347
juliennuellas <julien.nuellas@gmail.com>
2018-11-29 10:49:25 +0100

    add favicon to tutoriel-blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,12 +12,12 @@ STATIC_PATHS = [
     'images',
     'extra/robots.txt',
     'extra/CNAME',
-    'extra/favicon.png',
+    'extra/favicon.ico',
 ]
 EXTRA_PATH_METADATA = {
     'extra/robots.txt': {'path': 'robots.txt'},
     'extra/CNAME': {'path': 'CNAME'},
-    'extra/favicon.png': {'path': 'favicon.png'}
+    'extra/favicon.ico': {'path': 'favicon.ico'}
 }
 
 TIMEZONE = 'Europe/Paris'
